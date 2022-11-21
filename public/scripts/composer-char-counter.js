ced00fc2ffc936cfa2862a86dc35b47709630b6b
ive-m <ivesita.maria@gmail.com>
2022-11-21 10:21:05 -0500

    Add tweet container

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,19 +1,10 @@
-$(document).ready(function() {
-console.log('READY');
-
-const textarea = document.querySelector("#tweet-text");
-
-textarea.addEventListener("keydown", function(event){
-
- const maxLength = this.getAttribute("maxlength");
- const currentLength = event.target.value.length;
-
-  $("#counter").text(maxLength-currentLength);
-
-  if (currentLength >= maxLength) {
-   textarea.removeEventListener("keydown", this );
-  }
-
-  console.log(`${maxLength - currentLength} chars left`);
-});
+$(document).ready(function () {
+  console.log('READY');
+  const textarea = document.querySelector("#tweet-text");
+  textarea.addEventListener("input", function (event) {
+    const maxLength = 140;
+    const currentLength = event.target.value.length;
+    $("#counter").text(maxLength - currentLength);
+    console.log(`${maxLength - currentLength} chars left`);
+  });
 });
